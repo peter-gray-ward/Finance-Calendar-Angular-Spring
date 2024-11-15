0f0eb047a83b4c873e759ed90b6c134e462a79c0
PeterWard <peterward.pgw@gmail.com>
2024-11-14 21:40:02 -0500

    cleaning up

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electron', {
-  sync: (event)         => ipcRenderer.invoke('sync', event),
-  api: (which, data)    => ipcRenderer.invoke('api', which, data),
-  render: (which, data) => ipcRenderer.invoke('render', which, data)//,
-  // stream: (which, data) => ipcRenderer.invoke('stream', which, data)
-});
